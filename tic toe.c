bc4a12bd1d18fb6683b75476a076e781982a88ad
Nehal Singla <nehalsingla24@gmail.com>
2019-08-06 10:18:17 +0530

    second commit

--- tic toe.c
+++ tic toe.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<windows.h>
+#include<stdlib.h>
 # define rows 5
 # define colm 11
+
 
 void create_matrix(char arr[rows][colm] )
 {
@@ -28,7 +30,7 @@ void create_matrix(char arr[rows][colm] )
 }
    void print_matrix(char arr[rows][colm])
    {
-      system("CLS");
+      system("cls");
        for(int i=0;i<rows;i++)
        {
 
@@ -88,13 +90,96 @@ void create_matrix(char arr[rows][colm] )
 
        }
    }
+       int check(char arr[rows][colm])
+      {
+       if(arr[0][1]=='0' && arr[2][1]=='0')
+       {
+           if(arr[4][1]=='0')
+            return 1;
+
+       }
+        if(arr[0][1]=='0' && arr[0][5]=='0')
+       {
+           if(arr[0][9]=='0')
+            return 1;
+
+       }
+        if(arr[0][5]=='0' && arr[2][5]=='0')
+       {
+           if(arr[4][5]=='0')
+            return 1;
+
+       }
+        if(arr[2][1]=='0' && arr[2][5]=='0')
+       {
+           if(arr[2][9]=='0')
+           return 1;
+
+       }
+        if(arr[4][1]=='0' && arr[4][5]=='0')
+       {
+           if(arr[4][9]=='0')
+           return 1;
+
+       }
+        if(arr[0][9]=='0' && arr[2][9]=='0')
+       {
+           if(arr[4][9 ]=='0')
+           return 1;
+
+       }
+
+
+       // second player
+
+
+        if(arr[0][1]=='X' && arr[2][1]=='X')
+       {
+           if(arr[4][1]=='X')
+            return 2;
+
+       }
+        if(arr[0][1]=='X' && arr[0][5]=='X')
+       {
+           if(arr[0][9]=='X')
+            return 2;
+
+       }
+        if(arr[0][5]=='X' && arr[2][5]=='X')
+       {
+           if(arr[4][5]=='X')
+            return 2;
+
+       }
+        if(arr[2][1]=='X' && arr[2][5]=='X')
+       {
+           if(arr[2][9]=='X')
+           return 2;
+
+       }
+        if(arr[4][1]=='X' && arr[4][5]=='X')
+       {
+           if(arr[4][9]=='X')
+           return 2;
+
+       }
+        if(arr[0][9]=='X' && arr[2][9]=='X')
+       {
+           if(arr[4][9 ]=='X')
+           return 2;
+       }
+ }
 
 
 int main()
 {
-//start:
-    int num1,num2;
-    char arr[rows][colm];
+     int num1,num2;
+    char choice;
+     int b;
+       char arr[rows][colm];
+    start:
+
+
     create_matrix(arr);
     char name1[20];
     char name2[20];
@@ -111,6 +196,81 @@ int main()
         scanf("%d",&num1);
        fill_option(arr,1,num1);
         print_matrix(arr);
+      b = check(arr);
+        if(b==1)
+        {
+            printf("Player 1 wins\n");
+            printf("Do u want to continue(y/n) : ");
+            scanf("%c",choice);
+            if(choice=='y')
+            {
+                system("cls");
+                goto start;
+            }
+                else
+                {
+                    printf("game over");
+                    break;
+
+
+                }
+
+
+        }
+        if(b==2)
+        {
+            printf("Player 2 wins\n");
+            printf("Do u want to continue(y/n) : ");
+            scanf("%c",choice);
+            if(choice=='y')
+            {
+                 system("cls");
+                goto start;
+            }
+
+               else
+                    printf("Game over");
+                    break;
+        }
+
+       printf("player 2 :\n");
+        printf("enter ur choice : ");
+        scanf("%d",&num1);
+       fill_option(arr,2,num1);
+        print_matrix(arr);
+        b = check(arr);
+        if(b==1)
+        {
+            printf("Player 1 wins\n");
+            printf("Do u want to continue(y/n) : ");
+            scanf("%c",choice);
+            if(choice=='y')
+                 {
+                     system("cls");
+                goto start;
+                 }
+               else
+                 printf("Game over");
+                break;
+
+
+        }
+        if(b==2)
+        {
+            printf("Player 2 wins\n");
+            printf("Do u want to continue(y/n) : ");
+            scanf("%c",choice);
+            if(choice=='y')
+            {
+                 system("cls");
+                goto start;
+            }
+
+               else
+                    printf("Game over");
+                    break;
+        }
+
 
     }
 
